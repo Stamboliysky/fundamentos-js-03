2563c516f5eb0b4f110f7e31d27c7abcec461723
Stamboliysky <jordanspassoff@gmail.com>
2023-11-05 21:09:51 +0100

    Añadir ejercicio-07

--- src/ejercicio-07/index.js
+++ src/ejercicio-07/index.js
@@ -1,1 +1,5 @@
-// Escribe tu código aquí:
+// Ejercicio-07:
+const sentence = 'El perro es fiel y el gato es independiente.'
+const replacedSentence = sentence.replace
+('perro','pajaro')
+console.log(replacedSentence)
