14b540078c4c32a9c3598b47575a9ed1a1e076cf
Stamboliysky <jordanspassoff@gmail.com>
2023-11-13 21:28:21 +0100

    Añadir ejercicio-29

--- src/ejercicio-29/index.js
+++ src/ejercicio-29/index.js
@@ -1,1 +1,6 @@
-// Escribe tu código aquí:
+// Ejercicio 29: Crear Nuevo Array con filter
+
+const numbers = [1,2,3,4,5,6,7,8]
+const evenNunbers = numbers.filter((num) =>num % 2 === 0 )
+
+console.log(evenNunbers)
